c7d48cbbd193eb1cbcf883660f59c4779c552d0b
zhin abubakr <zheen99abubakr@gmail.com>
2023-03-10 22:35:14 +0300

    add searchBar and filter by region name

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router";
 import Layout from "./components/Layout/Layout";
 import Countries from "./components/Countries/Countries";
 import CountryDetails from "./components/CountryDetails/CountryDetails";
 import FilterSection from "./components/FilterSection/FilterSection";
-import { useEffect, useState } from "react";
 
 let COUNTRIES = [];
 
 function App() {
   const [loading, setIsLoading] = useState(false);
+  const [region, setRegion] = useState("");
+  const [inputValue, setInputValue] = useState("");
   const [currentCountries, setCurrentCountries] = useState([]);
 
   useEffect(() => {
@@ -21,6 +23,25 @@ function App() {
     });
   }, []);
 
+  const handleRegionChange = (e) => {
+    setInputValue("");
+    const countriesCopy = [...COUNTRIES];
+    const filteredCountry = countriesCopy.filter(
+      (country) => country.region === e.target.value
+    );
+    setCurrentCountries(filteredCountry);
+    setRegion(e.target.value);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    setRegion("");
+    const countriesCopy = [...COUNTRIES];
+    const result = countriesCopy.filter((country) =>
+      country.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setCurrentCountries(result);
+  };
   return (
     <Layout>
       <Routes>
@@ -29,8 +50,17 @@ function App() {
           exact
           element={
             <>
-              <FilterSection />
-              <Countries loading={loading} countriesData={currentCountries} />
+              <FilterSection
+                region={region}
+                onRegionChange={(region) => handleRegionChange(region)}
+                inputValue={inputValue}
+                onInputChange={handleInputChange}
+              />
+              <Countries
+                selectedRegion={region}
+                loading={loading}
+                countriesData={currentCountries}
+              />
             </>
           }
         />
